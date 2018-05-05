2f3329945a59540fa722e082906a94f80c4dd5cd
Brian Nagata <btnagata@gmail.com>
2018-05-04 22:16:35 -0700

    adding obama requests

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import os
-
 import requests
-from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
+from flask import Flask, Response, send_file
+
+
+ENDPOINT = 'http://talkobamato.me'
 
 app = Flask(__name__)
 
 
 def get_fact():
-
     response = requests.get("http://unkno.com")
-
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
-
-    return facts[0].getText()
+    return facts[0].getText().strip()
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    data = {
+        'input_text': get_fact(),
+    }
+    r = requests.post(url='/'.join([ENDPOINT, 'synthesize.py']),
+                      data=data,
+                      allow_redirects=False)
+    return r.headers['Location']
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
